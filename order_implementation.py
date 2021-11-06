d98ac9bf8ce11836399673454e9f865f383e2e58
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-06 23:48:29 +0800

    Replace new_building return with placeholder

--- order_implementation.py
+++ order_implementation.py
@@ -4,4 +4,4 @@ from main import GSRPG_DATA_FILE_PATH
 gsrpg = json.load(open(GSRPG_DATA_FILE_PATH,'r',encoding='UTF-8'))
 
 def new_building(type: str, quantity: int):
-    return {'construction-turns'}
+    pass
