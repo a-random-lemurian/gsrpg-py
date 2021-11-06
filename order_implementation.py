209eb2ef837ece090a8fd696090f8e5264a313e3
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-06 23:30:18 +0800

    Create order_implementation.py

---
+++ order_implementation.py
@@ -0,0 +1,7 @@
+import json
+from main import GSRPG_DATA_FILE_PATH
+
+gsrpg = json.load(open(GSRPG_DATA_FILE_PATH,'r',encoding='UTF-8'))
+
+def new_building(type: str, quantity: int):
+    return {'construction-turns'}
